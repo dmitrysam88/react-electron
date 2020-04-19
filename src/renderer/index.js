4ad1fc21b341609e014b67689ce2850c4a3c1a20
Sam Diy <sam88diy@gmail.com>
2020-04-19 21:53:38 +0300

    first commit

---
+++ src/renderer/index.js
@@ -0,0 +1,7 @@
+// Initial welcome page. Delete the following line to remove it.
+'use strict'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById("app"));
